46c01a47d28857dece4a89590b6d99b874cf26fd
adorahelen <adorahelenmin@gmail.com>
2024-09-27 14:50:08 +0900

    Initial commit

---
+++ src/main/java/kdt/hackathon/applysecurity/ApplySecurityApplication.java
@@ -0,0 +1,13 @@
+package kdt.hackathon.applysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplySecurityApplication.class, args);
+    }
+
+}
