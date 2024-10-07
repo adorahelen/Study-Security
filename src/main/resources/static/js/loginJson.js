b2c09cabbfdb40cd99fc6b769570193a57e3a3e2
adorahelen <adorahelenmin@gmail.com>
2024-10-07 09:48:07 +0900

    oauth2 + token + js

--- src/main/resources/static/js/loginJson.js
+++ src/main/resources/static/js/loginJson.js
@@ -8,15 +8,27 @@ if (createButton) {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                username: document.getElementById('username').value, // username으로 수정
-                password: document.getElementById('password').value, // password로 수정
+                username: document.getElementById('username').value, // 유저네임 입력값
+                password: document.getElementById('password').value, // 패스워드 입력값
             }),
         }).then((response) => {
             if (response.ok) {
-                alert('Login successful.');
-                location.replace(`/service`);
+                // Authorization 헤더에서 토큰을 가져옵니다.
+                const accessToken = response.headers.get("Authorization");
+                if (accessToken) {
+                    // 'Bearer ' 이후의 문자열을 추출합니다.
+                    const token = accessToken.substr(7);
+                    // 액세스 토큰을 로컬 스토리지에 저장
+                    localStorage.setItem('access_token', token);
+
+                    alert('Login successful.');
+                    location.replace(`/articles?token=`+token)
+                } else {
+                    alert('Login failed: Access token not received.');
+                }
             } else {
                 alert('Login failed: ' + response.statusText);
+                return Promise.reject('Login failed');
             }
         }).catch((error) => {
             console.error('Error during login:', error);
